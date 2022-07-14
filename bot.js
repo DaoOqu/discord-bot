d554bc1785ef0edd65d15b8e6a21a981b2561c08
David Oquendo <davidoqu23@gmail.com>
2022-07-13 22:05:53 -0500

    actually made sure it's working this time

--- bot.js
+++ bot.js
@@ -6,3 +6,5 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
+
+client.login(process.env.DISCORD_TOKEN);
