abfec5c60553686c0670abf12695bb568f1b234a
David Oquendo <davidoqu23@gmail.com>
2022-07-18 20:24:54 -0500

    Testing bot message replies

--- bot.js
+++ bot.js
@@ -7,4 +7,8 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on('ready', (msg) => {
+  if(msg.content.toLowerCase() === 'ping') msg.reply("pong!")
+});
+
 client.login(process.env.DISCORD_TOKEN);
