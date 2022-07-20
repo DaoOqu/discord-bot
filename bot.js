97eb22885ec26452bebf802c21f1053bc7987bb6
David Oquendo <davidoqu23@gmail.com>
2022-07-19 21:06:24 -0500

    Added cat command reply but need to create slash commands -> commented out for now

--- bot.js
+++ bot.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
+const { request } = require('undici');
 
-const {Client, Intents} = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+async function getJSONResponse(body) {
+  let fullBody = '';
+
+  for await(const data of body) {
+    fullBody += data.toString();
+  }
+
+  return JSON.parse(fullBody);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -9,6 +20,19 @@ client.on('ready', () => {
 
 client.on('messageCreate', (msg) => {
   if(msg.content.toLowerCase() === 'ping') msg.reply("pong!");
+});
+
+client.on('interactionCreate', async interaction => {
+  if(!interaction.isCommand()) return;
+  
+  const { commandName } = interaction;
+  await interaction.deferReply();
+
+  if(commandName.toLowerCase() === 'cat') {
+    const catResult = await request('https://aws.random.cat/meow');
+    const { file } = await getJSONResponse(catResult.body);
+    interaction.editReply({ files: [file] });
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
