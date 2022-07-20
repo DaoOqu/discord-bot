7242a827433ed30607efed0ccd5d45f9dbfbbab5
David Oquendo <davidoqu23@gmail.com>
2022-07-19 20:37:09 -0500

    got the bot to respond properly

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 
 const {Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('ready', (msg) => {
-  if(msg.content.toLowerCase() === 'ping') msg.reply("pong!")
+client.on('messageCreate', (msg) => {
+  if(msg.content.toLowerCase() === 'ping') msg.reply("pong!");
 });
 
 client.login(process.env.DISCORD_TOKEN);
